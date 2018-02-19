fd6198794099bfb67d2ad6719c62605a030d4e59
Ribake Ikumoluyi <reiybake05@googlemail.com>
2018-02-19 23:40:14 +0000

    Fixed stability issue with opening of new tab by adding waits

--- src/test/java/stepsdefinition/SwitchCommands.java
+++ src/test/java/stepsdefinition/SwitchCommands.java
@@ -1,5 +1,6 @@
 package stepsdefinition;
 
+import com.thoughtworks.selenium.Wait;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -8,10 +9,14 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.nio.channels.Pipe;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ribake on 15/02/2018.
@@ -47,7 +52,7 @@ public class SwitchCommands {
             driver.switchTo().window(s);
             System.out.println("Current window " + s + " has title "+ driver.getTitle());
         }
-
+        // close the latest window
         driver.close();
     }
 
@@ -56,10 +61,25 @@ public class SwitchCommands {
         String currentHandle = driver.getWindowHandle();
         System.out.println("Current Window Handle " + currentHandle);
 
-        // TODO: fix instability issue with buttons not loading before code tries to search this list
-        List<WebElement> allButtons = driver.findElements(By.tagName("button"));
-        // this opens an additional tab - which is also seen as a window
-        allButtons.get(2).click();
+        /* Add a wait to fix instability issue with buttons not loading before
+          code reaches this point */
+        // Option 1: Wait for button using the global implicit wait
+        // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Option 2: Wait for button using an explicit wait on the button
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement newTabButton = wait.until(ExpectedConditions.visibilityOf(driver.findElements(
+                                                                                By.tagName("button")).get(2)));
+        newTabButton.click();
+
+//        List<WebElement> allButtons = driver.findElements(By.tagName("button"));
+//        /* System.out.println("All buttons in DOM: " + allButtons.size());
+//        for (int i = 0; i < allButtons.size(); i++){
+//            System.out.println("Button " + (i + 1) + allButtons.get(i).getText());
+//        } */
+//        // this opens an additional tab - which is also seen as a window
+//
+//        allButtons.get(2).click();
 
         // get handles of all open windows
         Set<String> handles = driver.getWindowHandles();
@@ -73,13 +93,15 @@ public class SwitchCommands {
 
     @Then("^I switch to the new tab$")
     public void i_switch_to_the_new_tab() throws Throwable {
-        // Switch WebDriver on to the other tab(s)
+        // Switch WebDriver to the newly open tab
         String currentHandle = driver.getWindowHandle();
         for (String h : driver.getWindowHandles()){
             if (!h.equals(currentHandle)){
                 driver.switchTo().window(h);
             }
         }
+        // assert on the page title of the new tab
+        Assert.assertEquals(driver.getTitle(), "Selenium Framework | Selenium, Cucumber, Ruby, Java et al.");
 
     }
 }
